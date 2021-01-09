7a1a418921dc55a2ca2b0f2120a7dc272a503d2a
DurgaTV <DurgaTV>
2021-01-09 10:18:04 +0100

    new string file

---
+++ org/test/StringOccurence.java
@@ -0,0 +1,30 @@
+package org.test;
+
+public class StringOccurence {
+
+
+		public static boolean isEmpty(String s) {
+	        return s == null || s.length() == 0;
+	    }
+	 
+	    /* Counts how many times the substring appears in the larger string. */
+	    public static int countMatches(String text, String str) {
+	        if (isEmpty(text) || isEmpty(str)) {
+	            return 0;
+	        }
+	 
+	        return text.split(str, -1).length - 1;
+	    }
+	 
+	    public static void main(String[] args) {
+	 
+	        String text = "AABCCAAADCBBAADBBC";
+	        String str = "AA";
+	 
+	        int count = countMatches(text, str);
+	        System.out.println(count);
+	    }
+
+	}
+
+
